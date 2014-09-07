9039bb45b92209d24cdab8ee279a5ae70fa3ed41
Charles Fogel <cjfogel@gmail.com>
2014-09-06 20:52:16 -0700

    Project 1 EDA

--- plot1.r
+++ plot1.r
@@ -1,5 +1,3 @@
-
-setwd("C:/Users/Charles/Desktop/R/EDA/Project1")
 png("plot1.png", height = 480, width= 480)
 temp <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
 f <- temp[min(which(temp$Date == "1/2/2007")):max(which(temp$Date == "2/2/2007")),]
